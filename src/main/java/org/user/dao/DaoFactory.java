639d8b44f5aa37ac5709bf2c6020159bd7f0e3a8
nakyeonko3 <badaclock@gmail.com>
2024-03-12 03:57:53 +0900

    Refactor :"1장 오브젝트와 의존관계: 어플리케이션 컨텍스트 적용"

--- src/main/java/org/user/dao/DaoFactory.java
+++ src/main/java/org/user/dao/DaoFactory.java
@@ -1,11 +1,24 @@
 package org.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.user.connection.ConnectionMaker;
 import org.user.connection.DConnectionMaker;
 
+
+// @Configuration는 빈 펙토리를 위한 오브젝트 설정을 담당하는 클래스임을 알려줌
+@Configuration
 public class DaoFactory {
+
+//  오브젝트를 만들어주는 메소드에 @Bean을 붙여준다.
+    @Bean
     public UserDao userDao(){
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        return new UserDao(connectionMaker);
+        return new UserDao(connectionMaker());
     }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
+
 }
--- src/main/java/org/user/example/UserDaoTest.java
+++ src/main/java/org/user/example/UserDaoTest.java
@@ -1,5 +1,6 @@
 package org.user.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.user.dao.DaoFactory;
 import org.user.dao.UserDao;
 import org.user.entity.User;
@@ -9,7 +10,14 @@ import java.sql.SQLException;
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        UserDao userDao = new DaoFactory().userDao();
+
+//        //AnnotationConfigApplicationContext가 @Configuration이 붙은 코드를 설정 정보로 등록한다
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        //getBean은 빈으로 등록된 userDao를 호출한다. UserDao의 객체를 가져오는셈
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+
+//        UserDao userDao = new DaoFactory().userDao();
 
         User user = new User("whiteship", "백기선", "spring");
         userDao.add(user);
