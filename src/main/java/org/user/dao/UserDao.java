2455b4fe0c27e4e5f5e81a68a3d066ae08ddf980
nakyeonko3 <badaclock@gmail.com>
2024-03-21 01:16:01 +0900

    Feat :"2장 테스팅: @Before 적용"

--- src/main/java/org/user/dao/UserDao.java
+++ src/main/java/org/user/dao/UserDao.java
@@ -1,5 +1,6 @@
 package org.user.dao;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.user.entity.User;
 
 import javax.sql.DataSource;
@@ -15,7 +16,7 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user)throws SQLException {
+    public void add(User user) throws SQLException {
         Connection connection = dataSource.getConnection();
         PreparedStatement ps = connection.prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -31,15 +32,19 @@ public class UserDao {
     public User get(String id) throws SQLException {
         Connection connection = dataSource.getConnection();
         PreparedStatement ps = connection.prepareStatement("select * from users where id =?");
-        ps.setString(1,id);
+        ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
-        User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("passWord"));
+
+        User user = null;
+        if (rs.next()) {
+            user = new User(rs.getString("id"), rs.getString("name"), rs.getString("passWord"));
+        }
         rs.close();
         ps.close();
         connection.close();
 
+        if (user == null) throw new EmptyResultDataAccessException(1);
         return user;
     }
 
--- src/main/java/org/user/dao/UserDaoTest.java
+++ src/main/java/org/user/dao/UserDaoTest.java
@@ -1,9 +1,11 @@
 package org.user.dao;
 
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.user.entity.User;
 
 import java.sql.SQLException;
@@ -12,21 +14,29 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 
+
 public class UserDaoTest {
+    private UserDao userDao;
+
+    @Before
+    public void setUp(){
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        this.userDao = context.getBean("userDao", UserDao.class);
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void getUserFail() throws SQLException{
+        userDao.deletAll();
+        assertThat(userDao.getCount(),is(0));
+
+        userDao.get("unknown_id");
+    }
+
 
     @Test
     public void addAndGet() throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        //getBean은 빈으로 등록된 userDao를 호출한다. UserDao의 객체를 가져오는셈
-        UserDao userDao = context.getBean("userDao", UserDao.class);
-
-
-
         User user1 = new User(".gyumee", "박상철", "springno1");
         User user2 = new User("leegw700", "이길원", "springno2");
-        User user3 = new User("bumjin", "박범진", "springno3");
 
         userDao.deletAll();
         assertThat(userDao.getCount(), is(0));
@@ -37,9 +47,13 @@ public class UserDaoTest {
         userDao.add(user2);
         assertThat(userDao.getCount(), is(2));
 
-        userDao.add(user3);
-        assertThat(userDao.getCount(), is(3));
+        User userget1 = userDao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassWord(),is(user1.getPassWord()));
 
+        User userget2 = userDao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassWord(), is(user2.getPassWord()));
     }
 
 }
