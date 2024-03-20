7964bf85d72cb0b88ff219c0b918042db8291180
nakyeonko3 <badaclock@gmail.com>
2024-03-20 21:20:27 +0900

    Feat :"2장 테스팅: UserDao에 getAll, deleteAll 추가됨"

--- src/main/java/org/user/dao/UserDaoTest.java
+++ src/main/java/org/user/dao/UserDaoTest.java
@@ -22,16 +22,24 @@ public class UserDaoTest {
         //getBean은 빈으로 등록된 userDao를 호출한다. UserDao의 객체를 가져오는셈
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
+
+
+        User user1 = new User(".gyumee", "박상철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+        User user3 = new User("bumjin", "박범진", "springno3");
+
         userDao.deletAll();
         assertThat(userDao.getCount(), is(0));
 
-        User user = new User(".gyumee", "박상철", "springno1");
-        userDao.add(user);
+        userDao.add(user1);
         assertThat(userDao.getCount(), is(1));
 
-        User user2 = userDao.get(user.getId());
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassWord(), is(user.getPassWord()));
+        userDao.add(user2);
+        assertThat(userDao.getCount(), is(2));
+
+        userDao.add(user3);
+        assertThat(userDao.getCount(), is(3));
+
     }
 
 }
